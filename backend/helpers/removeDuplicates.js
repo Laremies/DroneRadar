e52dd44b6426c1e18a30d5f06c160cef20d87dd3
Laremies <laurensvanderhelm@live.com>
2023-01-09 03:29:25 +0200

    deleted some semicolons

--- backend/helpers/removeDuplicates.js
+++ backend/helpers/removeDuplicates.js
@@ -1,10 +1,10 @@
 const removeDuplicates = (drones) => {
   return drones.reduce((acc, curr) => {
     if (!acc.find(drone => drone.serialNumber === curr.serialNumber)) {
-      acc.push(curr);
+      acc.push(curr)
     }
-    return acc;
-  }, []);
+    return acc
+  }, [])
 }
 
 module.exports = removeDuplicates
