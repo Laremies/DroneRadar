a9cac878c1270a3282be69100e2e66045977475b
Laremies <laurensvanderhelm@live.com>
2023-01-10 16:47:47 +0200

    changed 'forEach'-loop to 'for-of'-loop so that async calls work correctly and added error handling when making requests

--- backend/controllers/drones.js
+++ backend/controllers/drones.js
@@ -10,44 +10,49 @@ let violationDrones = []
 
 // Get drones which violate the NDZ
 const getViolations = async () => {
-  const res = await axios.get('https://assignments.reaktor.com/birdnest/drones')
-  const json = convert.xml2js(res.data, {compact: true})
-  const drones = json.report.capture.drone
-  const radius = 100000
+  try {
+    const res = await axios.get('https://assignments.reaktor.com/birdnest/drones')
+    const json = convert.xml2js(res.data, {compact: true})
+    const drones = json.report.capture.drone
+    const radius = 100000
 
-  drones.forEach(async drone => {
-    const posX = drone.positionX._text
-    const posY = drone.positionY._text
-    const distance = calculateDistance(posX, posY)
+    for (const drone of drones) {
+      const posX = drone.positionX._text
+      const posY = drone.positionY._text
+      const distance = calculateDistance(posX, posY)
 
-    if (distance <= radius) {
-      const serialNum = drone.serialNumber._text
-      const violationDrone = violationDrones.find(d => d.serialNumber === serialNum)
+      if (distance <= radius) {
+        const serialNum = drone.serialNumber._text
+        const violationDrone = violationDrones.find(d => d.serialNumber === serialNum)
 
-      // If drone already exists, update its time and closest distance, else create a new drone.
-      if (violationDrone) {
-        violationDrone.time = Date.now()
+        // If drone already exists, update its time and closest distance, else create a new drone.
+        if (violationDrone) {
+          violationDrone.time = Date.now()
 
-        if (distance < violationDrone.closestDistance) {
-          violationDrone.closestDistance = distance
+          if (distance < violationDrone.closestDistance) {
+            violationDrone.closestDistance = distance
+          }
+        } else {
+          const pilotObject = await createPilot(serialNum)
+
+          const droneObject = {
+            serialNumber: serialNum,
+            closestDistance: distance,
+            time: Date.now(),
+            pilot: pilotObject
+          }
+
+          violationDrones.push(droneObject)
+          // redundancy for redundancy and race condition avoidance when using an array for storing data
+          violationDrones = removeDuplicates(violationDrones)
         }
-      } else {
-        const pilotObject = await createPilot(serialNum)
-
-        const droneObject = {
-          serialNumber: serialNum,
-          closestDistance: distance,
-          time: Date.now(),
-          pilot: pilotObject
-        }
-
-        violationDrones.push(droneObject)
-        // redundancy for weird cases where duplicates have appeared in the array
-        violationDrones = removeDuplicates(violationDrones)
       }
     }
-  })
-  violationDrones = deleteOldDrones(violationDrones)
+    violationDrones = deleteOldDrones(violationDrones)
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 
--- backend/helpers/createPilot.js
+++ backend/helpers/createPilot.js
@@ -2,17 +2,21 @@ const axios = require('axios')
 
 // Helper function to create a pilot object for a drone.
 const createPilot = async (serialNumber) => {
-  const res = await axios.get(`https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`)
-  const pilot = res.data
+  try {
+    const res = await axios.get(`https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`)
+    const pilot = res.data
 
-  const pilotObject = {
-    id: pilot.pilotId,
-    name: `${pilot.firstName} ${pilot.lastName}`,
-    phoneNum: pilot.phoneNumber,
-    email: pilot.email
+    const pilotObject = {
+      id: pilot.pilotId,
+      name: `${pilot.firstName} ${pilot.lastName}`,
+      phoneNum: pilot.phoneNumber,
+      email: pilot.email
+    }
+    return pilotObject
+    
+  } catch (error) {
+    console.log(`Error creating pilot: ${error.message}`)
   }
-  
-  return pilotObject
 }
 
 module.exports = createPilot
